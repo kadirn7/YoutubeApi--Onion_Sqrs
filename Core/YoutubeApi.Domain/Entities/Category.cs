0cf54ecf56f5ce502c8a65a63463d0602ca08791
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-11-25 19:30:59 +0300

    Temel Entitiler oluşturuldu

---
+++ Core/YoutubeApi.Domain/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Category:EntityBase,IEntityBase
+    {
+        public Category() { }
+        public Category(int parentId,string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty {  get; set; }
+        public ICollection<Detail> Details { get; set; }     //bire çok ilişki için eklenir.
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Detail:EntityBase
+    {
+        public Detail() { }
+        public Detail(string title,string description,int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }  
+    }
+}
