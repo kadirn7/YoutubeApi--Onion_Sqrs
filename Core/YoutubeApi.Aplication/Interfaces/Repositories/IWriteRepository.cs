7cd5cdc05f4e140c73e1e813be6a77066b4910af
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-11-28 10:24:57 +0300

    Write Repository added Project

--- Core/YoutubeApi.Aplication/Interfaces/Repositories/IWriteRepository.cs
+++ Core/YoutubeApi.Aplication/Interfaces/Repositories/IWriteRepository.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
 
 namespace YoutubeApi.Aplication.Interfaces.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> where T : class, IEntityBase, new()
     {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IList<T> entities);
+        Task<T> UpdateAsync(T entity);
+        
+        Task HardDeleteAsync(T entity);
 
-    }
+
+            }
 }
